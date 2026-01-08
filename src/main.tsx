5b572e0ef0b553b65f5557fc2af433d8819215e7
Akif Taskin <akiftaskin52@gmail.com>
2026-01-08 22:34:37 +0300

    amplify

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,21 @@ import { Amplify } from 'aws-amplify';
 import './index.css';
 import App from './App.tsx';
 
+const poolId = import.meta.env.VITE_COGNITO_USER_POOL_ID;
+const clientId = import.meta.env.VITE_COGNITO_CLIENT_ID;
+
+console.log('COGNITO ENV =>', { poolId, clientId });
+
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-      userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
+      userPoolId: poolId,
+      userPoolClientId: clientId,
     },
   },
 });
+
+console.log('AMPLIFY CONFIG =>', Amplify.getConfig());
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
